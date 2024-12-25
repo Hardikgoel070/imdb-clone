b547056ddbb61cd2cbc1f060c696c6d5cd43a336
Hardikgoel070 <b22bb048@iitj.ac.in>
2024-12-25 15:55:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Banner from './Components/Banner';
+import Trending from './Components/Trending';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Favourites from './Components/Favourites';
+function App() {
+  return (
+    <BrowserRouter>
+    <Navbar></Navbar>
+    <Routes>
+      <Route path='/' element={<><Banner/><Trending/></>}></Route>
+      <Route path='/Favourites' element={<><Favourites/></>}></Route> 
+      
+    </Routes>
+    
+    </BrowserRouter>
+  );
+}
+
+export default App;
